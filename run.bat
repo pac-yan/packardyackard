c541a60abbd84a2a9e370f1e30ba7312d202841e
ekarshner <elijahkarshner@gmail.com>
2019-08-21 03:26:46 -0400

    made a birthday cake in hell

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
 @echo off
-java -Xms9G -Xmx10G -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:+CMSIncrementalPacing -XX:ParallelGCThreads=2 -XX:+AggressiveOpts -jar server.jar
+java -Xms9G -Xmx10G -jar server.jar
